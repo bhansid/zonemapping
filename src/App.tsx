b3c1e6f5bc7a2941855a01e831eb502009aede29
Sid <bhansid@gmail.com>
2026-02-18 21:55:54 +0530

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Collect from "./pages/Collect";
 import Admin from "./pages/Admin";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/collect" element={<Collect />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="*" element={<Navigate to="/collect" />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/collect" element={<Collect />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="*" element={<Navigate to="/collect" />} />
+    </Routes>
   );
 }
